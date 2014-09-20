5fc016604e4ae9317eccfa59c0efa763c2032d3b
fersa <fsalazarwp@gmail.com>
2014-09-20 16:57:05 +0200

    not done yet but getting there

---
+++ server.R
@@ -0,0 +1,42 @@
+library('shiny')
+library('ggplot2')
+library('randomForest')
+dataset <- read.csv("data/dummyData1.csv", row.names=1)
+
+shinyServer(function(input, output) {
+    dataout <- reactive({
+      trainSize <- as.integer(input$trainRatio*nrow(dataset)/100)
+      datatrain <- dataset[c(1:trainSize),]
+      datatest <- dataset[(trainSize+1):nrow(dataset),]
+      rfModel <- randomForest(x=datatrain[,2:6], y=datatrain[,1],
+                              mtry=input$mtry, ntree=input$ntree, keep.forest=T)
+      predTest <- predict(rfModel, newdata=datatest[,c(2:6)])
+      dataout <- data.frame(dataset, 'Prediction' = c(rfModel$predicted, predTest), 
+                            'Error'=c((datatrain[,1]-rfModel$predicted),(datatest[,1]-predTest)))      
+    })
+  #########################################################
+    output$plot1 <- renderPlot({
+      p <- ggplot(dataout(), aes(x=X1, y=Y)) + 
+      geom_point(alpha=.3, size=5)
+      q <- p + geom_line(data=dataout(), aes(x=X1, y = Prediction))+
+      theme_bw()+geom_vline(xintercept = dataset[as.integer(input$trainRatio*nrow(dataset)/100),2])
+      print(q)
+    }, height=400)
+  ###########################################################
+    output$plot2 <- renderPlot({
+      r <- ggplot(dataout(), aes(x=X1, y=Error))+geom_point(stat='identity')+
+      theme_bw()+geom_vline(xintercept = dataset[as.integer(input$trainRatio*nrow(dataset)/100),2])
+      print(r)
+    }, height=200)
+  #########################################################
+    output$RMSE <- renderPrint({
+      trainSize <- as.integer(input$trainRatio*nrow(dataset)/100)  
+      c(sqrt(sum((dataout()$Error[1:trainSize])^2)/trainSize),
+      sqrt(sum((dataout()$Error[(trainSize+1):nrow(dataout())])^2)/(nrow(dataout())-trainSize)))
+    })
+  #########################################################
+    output$MaxErr <- renderPrint({
+      c(max(dataout()$Error[1:trainSize]),
+      max(dataout()$Error[(trainSize+1):nrow(dataout())]) )
+    })
+})
---
+++ ui.R
@@ -0,0 +1,25 @@
+library('shiny')
+library('ggplot2')
+library('randomForest')
+
+shinyUI(pageWithSidebar(
+  headerPanel("Build a Random Forest model"),
+  sidebarPanel(
+    h4('Percentage of data instances to be used for training. 
+       The rest is reserved for testing'),
+     sliderInput('trainRatio', 'Percentage of training data', min=1, max=100,
+                value=50, step=5, round=0),
+    h4('Parameters of the Random Forest Model'),
+    numericInput('mtry', 'mtry', min=1, max=6, value=2, step=1),
+    numericInput('ntree', 'Number of trees in the forest', min=100, max=1000, value=100, step=100)
+  ),
+  mainPanel(
+    h4('Results'),
+    h5('RMSE: Train - Test'),
+    verbatimTextOutput("RMSE"),
+    h5('Max(Abs(Error)): Train - Test'),    
+    verbatimTextOutput("MaxErr"),    
+    plotOutput('plot1'),
+    plotOutput('plot2')
+  )
+))
